1b6ebdd8dd8d38692f1ac648c8d1935f6a7d5a48
rvinodqa2009@gmail.com <rvinodqa2009@gmail.com>
2019-11-19 19:22:32 +0530

    First branch commit2

--- src/main/java/com/epam/ClassA.java
+++ src/main/java/com/epam/ClassA.java
@@ -4,6 +4,7 @@ public class ClassA {
     public static void main(String[] args) {
 		System.out.println("ClassE");
 		System.out.println("ClassF");
-	    	System.out.println("ClassG");
+	    System.out.println("ClassG");
+	    System.out.println("First Branch commit2");
 	}
 }
